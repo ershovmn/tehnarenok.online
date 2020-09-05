94f934648f42bad159b3e70702d7b7c38c9503f0
tehnarenok <ershov.mikhaill.nikolaevich@gmail.com>
2020-09-05 15:08:55 +0300

    измениение стилей для моб устройств

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Header.css'
 
 const Header = () => {
+    let [menuVisible, setMenuVisible] = useState(false);
+
+    document.body.style.overflow = 'auto'
+
+    if(document.body.clientWidth < 500) {
+        if(menuVisible) {
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 0
+            document.body.style.overflow = 'hidden'
+        }
+        return (
+            <>
+                <div className='header'>
+                    <a className='logo' href='/'> 
+                        <img src={require('../../img/logo2.png')} alt='logo' height='40px'/>
+                    </a>
+                    <div className='header-right' >
+                        <img onClick={() => setMenuVisible(!menuVisible)} src={require('../../img/menu.png')} height='40px'/>
+                    </div>
+                </div>
+                {menuVisible ? 
+                    <div className='header-menu'>
+                        <a href='/'>Главная</a>
+                        <a href='/projects'>Проектики</a>
+                        <a href='/blog'>Блог</a>
+                    </div>
+                : null }
+            </>
+        )
+    }
     return (
         <div className='header'>
             <a className='logo' href='/'> 
